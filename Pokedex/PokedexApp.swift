651193787906de732c02d2d99765edc89b4c5b8c
Rafaela Lourenço <Rafaela.Lourenco-ext@ab-Inbev.com>
2022-11-23 14:48:48 -0300

    Create a new project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by rafaela.lourenco on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
